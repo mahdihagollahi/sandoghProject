70d33813574bd39abb3c1ab818dc240989eeed85
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-07-11 12:14:40 +0330

    end of admin header

--- src/app/components/AdminHeader.tsx
+++ src/app/components/AdminHeader.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
-import useImage from '../favicon.ico'
+import useImage from '../assent/Img/40px.png'
 function AdminHeader() {
   return (
     <div>
         <div className='flex justify-between bg-[#FFFFFF] mt-4 mr-[31.9%] rounded-md shadow-2xl  w-2/3 py-3 px-6 h-16'>
         <div className='flex items-center'>
-            <p className='mt-2 text-[#2D3748] font-light text-lg '>
+            <p className='mt-1 text-[#2D3748] font-light text-lg '>
                 سلام ،  وقت شما بخیر  به بخش داشبورد خوش آمدید ، گزارشات مورد نیاز شما در اختیار شماست !
             </p>
         </div>
-        <div className='flex justify-between'>
+        <div className='flex justify-between gap-4'>
             <div>
-            <Image src={useImage} alt='user' width={40} height={40}/>\
+            <Image className='bg-[#FFFFFF] rounded-full ' src={useImage} alt='user' width={40} height={40}/>\
 
             </div>
-            <div>
-                
+            <div className='mt-2'>
+               <p className='text-[#2D3748] font-medium text-base '>
+               امیر محمد قنبری
+               </p>
             </div>
         </div>
         </div>
