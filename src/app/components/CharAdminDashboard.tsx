88f5041ef6df02d5e6a64a71683735f78fc4b8fb
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-07-17 21:00:45 +0330

    complete cha Admin dashboard

--- src/app/components/CharAdminDashboard.tsx
+++ src/app/components/CharAdminDashboard.tsx
@@ -1,70 +1,89 @@
 import React from 'react'
-import {Line} from 'react-chartjs-2'
-import {Chart as ChartJS , CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, scales } from 'chart.js'
+
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 function CharAdminDashboard() {
-    
-    const data ={
-        labels : ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور'],
-        datasets :[
-            {
-                label:"محتوا",
-                data: [60 , 55 , 50 , 70 , 65 , 80],
-                borderColor : 'rgba(54, 162, 235, 1)',
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                fill:false,
-                tension: 0.4
-            },
-            {
-                label: 'محتوا',
-                data: [60, 50, 45, 40, 35, 30],
-                borderColor: 'rgba(255, 99, 132, 1)',
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                fill: false,
-                tension: 0.4
-              }
+    const data = {
+        labels: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور'],
+        datasets: [
+          {
+            label: 'محتوا',
+            data: [120, 90, 60, 30, 10, 0],
+            borderColor: 'rgba(54, 162, 235, 1)',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            fill: false,
+            tension: 0.4
+          },
+          {
+            label: 'محتوا',
+            data: [120, 90, 60, 30, 10, 0],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            fill: false,
+            tension: 0.4
+          }
         ]
-    }
-
-    const option ={
-      responsive:true,
-      plugin:{
-        legend:{
-            position:'bottom'
+      };
+    
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom'
+          },
+          title: {
+            display: true,
+            // text: 'نمودار موجودی 6 ماه اول صندوق'
+          }
         },
-        title:{
-            display:true,
-            text:'نمودار موجودی 6 ماه اول صندوق'
-        }
-      },
-      scales:{
-        y:{
-            beginAtZerio:false,
-            ticks:{
-                callback:function(value){
-                    return value + 'م'
-                }
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              callback: function(value) {
+                return value + ' م';
+              }
             }
-        },
-        x:{
-            ticks:{
-                callback:function(value , index , values){
-                    return this.getLabelForValue(value)
-                }
+          },
+          x: {
+            ticks: {
+              callback: function(value, index, values) {
+                return this.getLabelForValue(value);
+              }
             }
+          }
         }
-      }
-    }
+      };
   return (
-  <div className='flex justify-center items-center mb-9'>
-    <div className=' w-96'>
-    <Line data={data} option={option} />
+    <div className='flex justify-center mt-2 items-center   md:justify-center  xl:justify-center xl:ml-14  xl:mt-2'>
+    <div className=' w-[630px] h-[440px] bg-white -mt-[460px]   shadow-xl rounded-lg px-4 '>
+        <div className='py-4 '>
+          <p className='font-medium text-sm text-[#000000]'>
+          نمودار موجودی 6ماه اول صندوق
+          </p>
+        </div>
+     <div>
+     <Line
+      
+      options={options}
+    data={data} 
+    
 
+    />
+
+     </div>
+  
     </div>
         </div>
   )
 }
 
 export default CharAdminDashboard
+
+
+
+
+
