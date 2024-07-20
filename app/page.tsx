e260ea1331f1d21f5b9a89c52e2b82a09b3a1587
developersamari <samsa8784@gmail.com>
2024-07-20 11:33:28 +0330

    creative support box and requsshion

--- app/page.tsx
+++ app/page.tsx
@@ -1,164 +1,95 @@
-import Image from 'next/image'
-import greenBackground from './Image.png'
+// import Image from "next/image";
+
+// import greenBackground from "./Image.png";
+
+// import React from 'react'
+
+// export default function Home() {
+//   return (
+//     <div className="relative h-[700px]">
+//       <div>
+//         <Image
+//           sizes="100vw"
+//           style={{
+//             width: "100%",
+//             height: "330px",
+//           }}
+//           src={greenBackground}
+//           alt=""
+//         />
+//       </div>
+//       <div className="bg-white flex justify-center content-center m-[80px] absolute top-[15px] left-[250px] rounded-[30px] shadow-md">
+//         <div className="w-[626px] h-[488px] rounded-[30px] p-[40px] shadow-md">
+//           <div className="flex flex-row-reverse gap-1 mr-[450px]">
+//             <div>
+//               <span>بازگشت</span>
+//             </div>
+//             <div>
+//               <span>
+//                 <svg
+//                   width="24"
+//                   height="24"
+//                   viewBox="0 0 24 24"
+//                   fill="none"
+//                   xmlns="http://www.w3.org/2000/svg"
+//                 >
+//                   <path
+//                     d="M4.25 11.7256L19.25 11.7256"
+//                     stroke="black"
+//                     strokeWidth="1.5"
+//                     strokeLinecap="round"
+//                     strokeLinejoin="round"
+//                   />
+//                   <path
+//                     d="M10.2988 5.701L4.24883 11.725L10.2988 17.75"
+//                     stroke="black"
+//                     strokeWidth="1.5"
+//                     strokeLinecap="round"
+//                     strokeLinejoin="round"
+//                   />
+//                 </svg>
+//               </span>
+//             </div>
+//           </div>
+
+//           <div className="mt-[50px] flex flex-col gap-4 mr-[40px]">
+//             <span className="text-[#4FD1C5] text-lg text-right">
+//               ورود به صندوق قرض الحسنه مهر
+//             </span>
+//             <p className="text-sm text-right opacity-55">
+//               به صندوق قرض الحسنه مهر خوش آمدید، لطفا برای ورود روی دکمه مورد
+//               نظر خود کلیک نمایید
+//             </p>
+//             <p className="text-sm text-right mt-[50px] opacity-55">
+//               اگر کاربر هستید روی این گزینه کلیک کنید
+//             </p>
+
+//               <button className="bg-[#4FD1C5] w-[420px] h-[56px] text-white rounded-[5px] ml-[87px] mt-4">
+//                 ورود کاربران
+
+//               </button>
+
+//            </div>
+//          </div>
+//        </div>
+
+//     </div>
+//   );
+// }
+
+import React from "react";
+
+
+
+import Requestbox from "./component/Requestbox"
+import Supportbox from "./component/Supportbox";
 export default function Home() {
   return (
-    <div>
-      <div>
-        <Image sizes="100vw" style={{
-        width: '100%',
-        height: 'auto',
-      }} src={greenBackground} alt="" />
+    <>
+    <div className="flex flex-row-reverse gap-[21px]">
+      <Supportbox/>
+      <Requestbox/>
       </div>
-      <div className="bg-white  flex justify-center content-center m-[80px] ">
-        <div className="w-[600px] h-[400px] rounded-[30px] bg-white p-[40px] shadow-md 	">
-          <div className="flex flex-row-reverse justify-between ">
-            <div className="text-[#4FD1C5]">
-              <span>ورود به صندوق قرض الحسنه مهر </span>
-            </div>
-            <div className="flex flex-row-reverse gap-1 ">
-              <div>
-                <span>بازگشت</span>
-              </div>
-              <div>
-                <span>
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4.25 11.7256L19.25 11.7256"
-                      stroke="black"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M10.2988 5.701L4.24883 11.725L10.2988 17.75"
-                      stroke="black"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
-}
-
-{
-  /* <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-  <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-    Get started by editing&nbsp;
-    <code className="font-mono font-bold">app/page.tsx</code>
-  </p>
-  <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-    <a
-      className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      By{" "}
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      />
-    </a>
-  </div>
-</div>
-
-<div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-  <Image
-    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    src="/next.svg"
-    alt="Next.js Logo"
-    width={180}
-    height={37}
-    priority
-  />
-</div>
-
-<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-  <a
-    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <h2 className="mb-3 text-2xl font-semibold">
-      Docs{" "}
-      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-        -&gt;
-      </span>
-    </h2>
-    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-      Find in-depth information about Next.js features and API.
-    </p>
-  </a>
-
-  <a
-    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <h2 className="mb-3 text-2xl font-semibold">
-      Learn{" "}
-      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-        -&gt;
-      </span>
-    </h2>
-    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-      Learn about Next.js in an interactive course with&nbsp;quizzes!
-    </p>
-  </a>
-
-  <a
-    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <h2 className="mb-3 text-2xl font-semibold">
-      Templates{" "}
-      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-        -&gt;
-      </span>
-    </h2>
-    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-      Explore starter templates for Next.js.
-    </p>
-  </a>
-
-  <a
-    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <h2 className="mb-3 text-2xl font-semibold">
-      Deploy{" "}
-      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-        -&gt;
-      </span>
-    </h2>
-    <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-      Instantly deploy your Next.js site to a shareable URL with Vercel.
-    </p>
-  </a>
-</div>
-</main> */
 }
