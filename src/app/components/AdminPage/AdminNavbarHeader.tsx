cccca9136d8ea4b175a7769fc6777b55a6391f1a
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-08-02 23:51:40 +0330

    add rout to navbar header

--- src/app/components/AdminPage/AdminNavbarHeader.tsx
+++ src/app/components/AdminPage/AdminNavbarHeader.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import Image from 'next/image'
 import groupImage from '@/src/app/assent/Img/adminPanel/Group 39513.svg'
+import Link from 'next/link'
 
 function AdminPageNavbar() {
   return (
     <div>
         <div className='flex items-center mt-10 mr-10  '>
+          <Link href='/dashboard'>
             <div className='flex gap-6 border-b-2 pb-9 w-[260px]'>
                 <div className=''>
                 <Image src={groupImage} width={48} height={48} alt='group'/>
@@ -19,6 +21,7 @@ function AdminPageNavbar() {
                 <div>
                 </div>
             </div>
+            </Link>
         </div>
     </div>
   )
