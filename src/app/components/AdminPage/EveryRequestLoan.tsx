ed4c063c26b0efde2c2e8385955bbe6656072e50
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-08-03 00:00:10 +0330

    fix header name

--- src/app/components/AdminPage/EveryRequestLoan.tsx
+++ src/app/components/AdminPage/EveryRequestLoan.tsx
@@ -50,17 +50,18 @@ const users: User[] = [
 const EveryRequestLoan: React.FC = () => {
   return (
     <div >
-         <div className='flex justify-between  items-center mb-2 mt-5 mr-3  '>
-            <div className='mr-2 '>
+         <div className='flex justify-between  items-center mb-2 mt-10 mr-3  '>
+         <div className='mr-2 '>
                 <p className='font-bold text-lg'>
-                مشاهده کاربران           
+                درخواست وام                
                 </p>
             </div>
            
 
 
-            <div className='flex justify-end mr-2  '>
-              <a href="">
+            <div  >
+              <a href="" className='flex items-center ml-7' >
+                بازگشت
               <Image
                 src={backImage}
                 width={38}
@@ -69,7 +70,6 @@ const EveryRequestLoan: React.FC = () => {
 
                 />
               </a>
-              
             </div>
         </div>
       <div>
