7825f4acb8a4a7676ffed62068c1b3c73d1bf5fb
developersamari <samsa8784@gmail.com>
2024-08-03 15:48:48 +0330

    edit landing and loagin

--- src/app/components/UserPage/buttompament.tsx
+++ src/app/components/UserPage/buttompament.tsx
@@ -7,11 +7,11 @@ export default function buttompament() {
   return (
     <>
     <div className="flex flex-row w-[268px] h-[56px] gap-2 bg-white rounded-md ">
-      <div className="flex flex-row gap-[3px]   ">
+      <div className="flex flex-row gap-[3px] hover:border-b-2 hover:border-green-600 hover:border-solid    ">
         <Image className="w-[18px] h-[16px] mt-[18px] " src={eshterak} alt="" />
-        <button className="text-[#9F9F9F]">اقساط اشتراک</button>
+        <button className="text-[#9F9F9F] ">اقساط اشتراک</button>
       </div>
-      <div className="flex flex-row gap-[3px]">
+      <div className="flex flex-row gap-[3px] hover:border-b-2 hover:border-green-600 hover:border-solid ">
         <Image className="w-[20px] h-[20px] mt-[18px]" src={vamghest} alt="" />
         <button className="text-[#9F9F9F]">اقساط وام</button>
       </div>
--- src/app/components/UserPage/landing.tsx
+++ src/app/components/UserPage/landing.tsx
@@ -2,37 +2,37 @@ import React from "react";
 import Image from "next/image";
 import loagin from "@/src/app/assent/Img/userPanel/loagin.png";
 import Group from "@/src/app/assent/Img/userPanel/Group 39519.svg";
-import loagimg from "@/src/app/assent/Img/userPanel/man.svg";
+import loagimg from "@/src/app/assent/Img/userPanel/menWithBollon.svg";
 
 export default function Landing() {
   return (
     <>
-      <div className="relative h-[500px]">
+      <div className="relative ">
         <div>
           <Image
             sizes="100vw"
             style={{
               width: "100%",
-              height: "260px",
+              height: "350px",
             }}
             src={loagin}
             alt=""
           />
         </div>
 
-        <div className="flex flex-row absolute gap-6 top-[10px] mr-[44px]">
+        <div className="flex flex-row absolute gap-6 top-[2%] mr-[2.5%]">
           <Image className=" w-[24px] h-6" src={Group} alt="" />
           <p className="font-bold	">صندوق قرض الحسنه مهر </p>
         </div>
         <div>
           <Image
-            className="top-[20px] right-[750px]  absolute"
+            className="top-[20%] right-[60%]  absolute"
             style={{ width: "503px", height: "298px" }}
             src={loagimg}
             alt=""
           />
         </div>
-        <div className="flex flex-col gap-[30px] mr-[44px] absolute top-[70px]">
+        <div className="flex flex-col gap-[30px] mr-[2.5%] absolute top-[16%]">
           <p>صندوق قرض الحسنه مهر ، با مهر در کنار هم </p>
           <p className="w-[550px]">
             ما در کنار جمع شده‌ایم تا در صندوق قرض الحسنه شریک باشیم ، هر یک از
@@ -40,11 +40,11 @@ export default function Landing() {
             بخواهند میتوانند مبلغ پیشنهادی خود را اعلام کنند و وام دریافت کنند .
           </p>
         </div>
-        <button className="bg-[#4FD1C5] w-[300px] h-[56px] text-white rounded-[5px] absolute top-[235px] mr-[44px]">
+        <button className="bg-[#4FD1C5] w-[300px] h-[56px] text-white rounded-[5px] absolute top-[45%] right-[2.5%]">
           ورود کاربران
         </button>
         <div>
-          <p className="w-[1080px] mr-[44px] mt-24 opacity-[60%]">
+          <p className="w-[1080px]  mr-[2.5%] mt-[10%] opacity-[60%]">
             برای عضویت در صندوق مهر به عنوان کاربر ، شما ابتدا باید با مدیر
             صندوق هماهنگ و اعلام عضویت کنید ، سپس نام کاربری و رمز شما در سیستم
             ثبت و آماده ورود به صندوق میشوید ، اگر نام کاربری و رمز شما تایید
