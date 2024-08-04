d1325c0769d379ca7ec2d206a88aab76f6afc99a
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-08-05 01:40:07 +0330

    add exit page

--- src/app/components/UserPage/Modalout.tsx
+++ src/app/components/UserPage/Modalout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-function Modalout() {
+function Modalout({handleCloseModal}) {
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-3 w-96 shadow-md p-8 rounded-md ">
@@ -9,7 +9,7 @@ function Modalout() {
         <p>آیا ازخروج خود اطمینان دارید؟</p>
         <div className="flex flex-row-reverse gap-2">
           <button className="text-red-500  py-2 px-10 hover:bg-[#FFF2F2] hover:rounded-md">خروج</button>
-          <button className="text-[#1D91CC] py-2 px-10 hover:bg-blue-100 hover:rounded-md">بازگشت</button>
+          <button className="text-[#1D91CC] py-2 px-10 hover:bg-blue-100 hover:rounded-md " onClick={handleCloseModal}>بازگشت</button>
         </div>
       </div>
     </>
