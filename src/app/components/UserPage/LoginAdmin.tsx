caabcdfc1c9ad782877fe080b1f9adeaf5a42538
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-08-19 23:54:23 +0330

    add modal to forget password

--- src/app/components/UserPage/LoginAdmin.tsx
+++ src/app/components/UserPage/LoginAdmin.tsx
@@ -1,10 +1,8 @@
-
-
 import React, { useState } from "react";
 import Image from "next/image";
 import greenBackground from "@/src/app/assent/Img/userPanel/GreenBackgound.png";
 import axios, { AxiosResponse } from "axios";
-import Link from "next/link";
+import Password2 from "@/src/app/components/UserPage/Password2"; 
 
 interface LoginResponse {
   token?: string;
@@ -16,6 +14,7 @@ const LoginAdmin: React.FC = () => {
   const [password, setPassword] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false); 
 
   const handleLogin = async () => {
     try {
@@ -44,7 +43,9 @@ const LoginAdmin: React.FC = () => {
     }
   };
 
-  
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
     <div className="relative h-[450px]">
       <div>
@@ -108,7 +109,7 @@ const LoginAdmin: React.FC = () => {
 
           <div className="flex flex-col gap-[20px] justify-center item-center mt-[50px] mr-[55px]">
             <div className="flex flex-col">
-            <span className="text-[#999999]">
+              <span className="text-[#999999]">
                 نام کاربری ، شماره ملی شما است
               </span>
               <input
@@ -117,11 +118,10 @@ const LoginAdmin: React.FC = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
-              
             </div>
 
             <div className="flex flex-col relative">
-            <span className="text-[#999999]">رمز عبور خود را وارد کنید</span>
+              <span className="text-[#999999]">رمز عبور خود را وارد کنید</span>
               <input
                 className="w-[420px] h-[42px] rounded-md p-3 border-[1px] border-[#E2E8F0]"
                 placeholder="رمز عبور"
@@ -135,8 +135,6 @@ const LoginAdmin: React.FC = () => {
                 onClick={() => setShowPassword(!showPassword)}
               >
                 {showPassword ? (
-
-
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -156,31 +154,41 @@ const LoginAdmin: React.FC = () => {
                       d="M3 3l18 18"
                     />
                   </svg>
-                 
                 ) : (
-               
-
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-</svg>
-
-                
-                  
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
                 )}
               </div>
               <div>
-                  <Link href='/forgetpassword'>
-                  <p className="text-[#394860] text-sm mt-1">
-                    فراموشی رمز؟
-                  </p>
-                  </Link>
-                </div>
+                <p
+                  className="text-[#394860] text-sm mt-1 cursor-pointer"
+                  onClick={handleOpenModal} // Open modal when clicking the link
+                >
+                  فراموشی رمز؟
+                </p>
+              </div>
             </div>
           </div>
           {errorMessage && (
             <div className="bg-[#FFF2F2] text-[#C30000] text-base font-normal text-center mt-4">
-               رمزعبور را اشتباه وارد کردید
+              رمزعبور را اشتباه وارد کردید
             </div>
           )}
 
@@ -192,7 +200,12 @@ const LoginAdmin: React.FC = () => {
           </button>
         </div>
       </div>
-     
+
+      {showModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <Password2 handleCloseModal={handleCloseModal} />
+        </div>
+      )}
     </div>
   );
 }
