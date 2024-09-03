c1e64da2e975ba335d3d4d77dc61e3983170ed0a
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-09-03 20:56:02 +0330

    fix loan aplication page api

--- src/app/components/AdminPage/DetailUserComponnent.tsx
+++ src/app/components/AdminPage/DetailUserComponnent.tsx
@@ -1,36 +1,179 @@
+// import React from "react";
+// import Image from "next/image";
+// import Image1 from "@/src/app/assent/Img/adminPanel/Avatar.svg";
+// import arrowImage from "@/src/app/assent/Img/adminPanel/back.svg";
+// import cardImage from "@/src/app/assent/Img/adminPanel/carddetail.svg";
+// import cardImage2 from "@/src/app/assent/Img/adminPanel/carddetail2.png";
+
+// const userDetail = [
+//   {
+//     id: 1,
+//     src: Image1,
+
+//     user: "الهه علی نیا",
+//     statuse: false,
+//     cardNumber: "6219 8619 111 5771",
+//     nationalNumber: "6219 8619 111 5771",
+//     name: "الهه",
+//     lastname: "علی نیا",
+//     phoneNumber: "09192803715",
+//     importantPhoneNumber: "02133168811",
+//     homeNumber: "012331007342",
+//     shaNumber: "52452435234523452345",
+//   },
+// ];
+
+// function DetailUser() {
+//   return (
+//     <div>
+//       <div className="flex  justify-between items-center mb-2 mt-9  ">
+//         <div className="mr-2 ">
+//           <p className="font-bold text-lg">حساب مدیریت</p>
+//         </div>
+//         <div className="flex justify-end mr-2  ">
+//           <a href="/everyuser" className="flex items-center">
+//             بازگشت
+//             <Image src={arrowImage} width={38} height={38} alt="arrow" />
+//           </a>
+//         </div>
+//       </div>
+
+//       <div className="bg-white  shadow-lg mt-5 px-28 py-20  cursor-pointer rounded-sm">
+//         <div className="flex justify-center">
+//           <Image
+//             src={userDetail[0].src}
+//             width={98}
+//             height={98}
+//             alt="user"
+//             className="rounded-sm"
+//           />
+//         </div>
+
+//         <div className="flex justify-center mt-5">
+//           <div>
+//             <p className="font-normal mr-6 text-[9px] text-[#2D3748]">
+//               شماره کارت
+//             </p>
+//             <Image src={cardImage} width={244} height={140} alt="cardNumber" />
+//           </div>
+
+//           <div>
+//             <p className="font-normal mr-6 text-[9px] text-[#2D3748]">
+//               کارت ملی
+//             </p>
+//             <Image src={cardImage2} width={244} height={140} alt="cardNumber" />
+//           </div>
+//         </div>
+
+//         <div className="mt-10">
+//           <div className="flex">
+//             <div className="relative ">
+//               <label className="absolute -top-2 z-10 left-[83%] px-3 bg-white py-2 dark:bg-black">
+//                 نام
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-[100%] md:w-96 h-14 text-black px-40 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[68%] px-3 bg-white py-2">
+//                 نام خانوادگی
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//           </div>
+//           <div className="flex">
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[67%] px-3 bg-white py-2">
+//                 شماره موبایل
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[58%] px-3 bg-white py-2">
+//                 شماره تلفن ضروری
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//           </div>
+
+//           <div className="flex">
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[72%] px-3 bg-white py-2">
+//                 تلفن منزل
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[76%] px-3 bg-white py-2">
+//                 کد ملی
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//           </div>
+
+//           <div className="flex">
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[71%] px-3 bg-white py-2">
+//                 شماره کارت
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//             <div className="relative w-full md:w-auto">
+//               <label className="absolute -top-2 z-10 left-[72%] px-3 bg-white py-2">
+//                 شماره شبا
+//               </label>
+//               <input
+//                 type="text"
+//                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default DetailUser;
+
+
+
 import React from "react";
 import Image from "next/image";
+import axios from "axios";
 import Image1 from "@/src/app/assent/Img/adminPanel/Avatar.svg";
 import arrowImage from "@/src/app/assent/Img/adminPanel/back.svg";
 import cardImage from "@/src/app/assent/Img/adminPanel/carddetail.svg";
 import cardImage2 from "@/src/app/assent/Img/adminPanel/carddetail2.png";
 
-const userDetail = [
-  {
-    id: 1,
-    src: Image1,
-
-    user: "الهه علی نیا",
-    statuse: false,
-    cardNumber: "6219 8619 111 5771",
-    nationalNumber: "6219 8619 111 5771",
-    name: "الهه",
-    lastname: "علی نیا",
-    phoneNumber: "09192803715",
-    importantPhoneNumber: "02133168811",
-    homeNumber: "012331007342",
-    shaNumber: "52452435234523452345",
-  },
-];
-
-function DetailUser() {
+function DetailUser({ userDetail }) {
   return (
     <div>
-      <div className="flex  justify-between items-center mb-2 mt-9  ">
-        <div className="mr-2 ">
+      <div className="flex justify-between items-center mb-2 mt-9">
+        <div className="mr-2">
           <p className="font-bold text-lg">حساب مدیریت</p>
         </div>
-        <div className="flex justify-end mr-2  ">
+        <div className="flex justify-end mr-2">
           <a href="/everyuser" className="flex items-center">
             بازگشت
             <Image src={arrowImage} width={38} height={38} alt="arrow" />
@@ -38,10 +181,10 @@ function DetailUser() {
         </div>
       </div>
 
-      <div className="bg-white  shadow-lg mt-5 px-28 py-20  cursor-pointer rounded-sm">
+      <div className="bg-white shadow-lg mt-5 px-28 py-20 cursor-pointer rounded-sm">
         <div className="flex justify-center">
           <Image
-            src={userDetail[0].src}
+            src={userDetail.avatar || Image1}
             width={98}
             height={98}
             alt="user"
@@ -67,12 +210,13 @@ function DetailUser() {
 
         <div className="mt-10">
           <div className="flex">
-            <div className="relative ">
+            <div className="relative">
               <label className="absolute -top-2 z-10 left-[83%] px-3 bg-white py-2 dark:bg-black">
                 نام
               </label>
               <input
                 type="text"
+                value={userDetail.name || ""}
                 className="border w-[100%] md:w-96 h-14 text-black px-40 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -82,6 +226,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.lastname || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -93,6 +238,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.phoneNumber || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -102,6 +248,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.importantPhoneNumber || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -114,6 +261,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.homeNumber || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -123,6 +271,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.nationalNumber || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -135,6 +284,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.cardNumber || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -144,6 +294,7 @@ function DetailUser() {
               </label>
               <input
                 type="text"
+                value={userDetail.shaNumber || ""}
                 className="border w-96 md:w-96 h-14 border-[#CACACA] rounded-md relative m-3"
               />
             </div>
@@ -152,6 +303,22 @@ function DetailUser() {
       </div>
     </div>
   );
+}
+
+// گرفتن اطلاعات از سرور با استفاده از getServerSideProps
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+
+  const response = await axios.post(
+    "https://mohammadelia30.ir/shabab/api/users/index",
+    { id }
+  );
+
+  return {
+    props: {
+      userDetail: response.data.data.user || {},
+    },
+  };
 }
 
 export default DetailUser;
--- src/app/components/AdminPage/EveryRequestLoan.tsx
+++ src/app/components/AdminPage/EveryRequestLoan.tsx
@@ -1,167 +1,3 @@
-// import React, { useState } from 'react';
-// import Image from 'next/image';
-// import axios from 'axios';
-// import { useQuery } from '@tanstack/react-query';
-// import backImage from '@/src/app/assent/Img/adminPanel/back.svg';
-// import EveryLoanAplicationTable from '@/src/app/components/AdminPage/EveryLoanAplicationTable';
-// import RoutRequestLoan from './RoutRequestLoan';
-// import SwichButton from './SwichButton';
-
-// interface Loan {
-//   id: number;
-//   name: string;
-//   amount: string;
-//   date: string;
-//   description: string;
-//   type: string;
-//   requestNumber: number;
-//   guarantors: string;
-// }
-
-// const fetchLoans = async () => {
-//   try {
-//     const token = localStorage.getItem('authToken');
-
-//     const response = await axios.post('https://mohammadelia30.ir/shabab/api/loans/show/admin', {
-//       count: 'all',
-//     }, {
-//       headers: {
-//         'Authorization': `Bearer ${token}`,
-//         'Content-Type': 'application/json'
-//       }
-//     });
-
-//     console.log('Response data:', response.data); 
-//     const { count, loans } = response.data;
-
-//     return {
-//       count, 
-//       loans: loans.map((loan: any) => ({
-//         id: loan.id,
-//         name: `${loan.user.first_name} ${loan.user.last_name}`,
-//         amount: `${loan.price.toLocaleString()} تومان`,
-//         date: new Date(loan.created_at).toLocaleDateString('fa-IR'),
-//         description: loan.user_description,
-//         type: loan.type === 'urgent' ? 'ضروری' : 'معمولی',
-//         requestNumber: loan.loan_number,
-//         guarantors: loan.guarantors_accept === 'accepted' ? 'نامشخص' : 'رحمانی'
-//       }))
-//     };
-//   } catch (error) {
-//     console.error('Error fetching loan data:', error);
-//     if (axios.isAxiosError(error)) {
-//       console.error('Error response data:', error.response?.data); // گزارش داده‌های خطا
-//       console.error('Error response status:', error.response?.status); // وضعیت خطا
-//     }
-//     throw error;
-//   }
-// };
-
-
-// const EveryRequestLoan: React.FC = () => {
-//   const [isUrgent, setIsUrgent] = useState(false);
-
-//   const { data, isLoading, isError, error } = useQuery({
-//     queryKey: ['loans'],
-//     queryFn: fetchLoans
-//   });
-
-//   const count = data?.count;
-//   const users = data?.loans || [];
-
-//   if (isLoading) {
-//     return (
-//       <div>
-        
-//         <div className='flex  justify-between items-center mb-2 mt-10 mr-3'>
-//           <div className='mr-2'>
-//             <p className='font-bold text-lg whitespace-nowrap dark:text-white'>درخواست وام</p>
-//           </div>
-//           <div className='mr-[210%]'>
-//             <a href="" className='flex  text-lg  dark:text-white items-center '>
-//               بازگشت
-//               <Image src={backImage} width={68} height={68} alt='arrow' />
-//             </a>
-//           </div>
-//         </div>
-//         <div className='flex gap-[47%] items-center'>
-//           <div>
-//             <RoutRequestLoan />
-//           </div>
-      
-//         </div>
-//         <div className='mt-4'>
-//         <span className="loading loading-dots text-accent loading-sm"></span>
-//         </div>
-//       </div>
-//     );
-//   }
-
-//   if (isError) {
-//     return (
-//       <div>
-//         <div className='flex justify-between items-center mb-2 mt-10 mr-3'>
-//           <div className='mr-2'>
-//             <p className='font-bold text-lg whitespace-nowrap'>درخواست وام</p>
-//           </div>
-//           <div className='mr-[210%]'>
-//             <a href="" className='flex  text-lg  dark:text-white items-center '>
-//               بازگشت
-//               <Image src={backImage} width={68} height={68} alt='arrow' />
-//             </a>
-//           </div>
-//         </div>
-//         <div className='flex gap-[47%] items-center'>
-//           <div>
-//             <RoutRequestLoan />
-//           </div>
-        
-//         </div>
-//         <div className='mt-4'>
-//           خطا در دریافت داده‌ها: {error instanceof Error ? error.message : 'مشخص نشده'}
-//         </div>
-//       </div>
-//     );
-//   }
-
-//   const filteredUsers = users.filter(user => isUrgent ? user.type === 'ضروری' : user.type === 'معمولی');
-
-//   return (
-//     <div>
-//       <div className='flex justify-between items-center mb-2 mt-10 mr-3'>
-//         <div className='mr-2'>
-//           <p className='font-bold text-lg'>درخواست وام</p>
-//         </div>
-//         <div>
-//           <a href="" className='flex items-center ml-7'>
-//             بازگشت
-//             <Image src={backImage} width={38} height={38} alt='arrow' />
-//           </a>
-//         </div>
-//       </div>
-//       <div className='flex gap-[47%] items-center'>
-//         <div>
-//           <RoutRequestLoan />
-//         </div>
-//         <div>
-//           <SwichButton setIsUrgent={setIsUrgent} />
-//         </div>
-//       </div>
-//       <div className='mt-4'>
-        
-//       </div>
-//       <div className='mt-4'>
-//         <EveryLoanAplicationTable users={filteredUsers} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default EveryRequestLoan;
-
-
-
-
 import React, { useState } from 'react';
 import Image from 'next/image';
 import axios from 'axios';
@@ -182,14 +18,16 @@ interface Loan {
   guarantors: string;
 }
 
-const fetchLoans = async () => {
+const fetchLoans = async (isUrgent: boolean) => {
   try {
     const token = localStorage.getItem('authToken');
+    const loanType = isUrgent ? 'neccessary' : 'normal';
 
     const response = await axios.post(
       'https://mohammadelia30.ir/shabab/api/loans/show/admin',
       {
         count: 'all',
+        type: loanType,
       },
       {
         headers: {
@@ -198,22 +36,19 @@ const fetchLoans = async () => {
         },
       }
     );
-     console.log(response.data)
-    const { count, loans } = response.data;
-        
-    return {
-      count,
-      loans: loans.map((loan: any) => ({
-        id: loan.id,
-        name: `${loan.user.first_name} ${loan.user.last_name}`,
-        amount: `${loan.price.toLocaleString()} تومان`,
-        date: new Date(loan.created_at).toLocaleDateString('fa-IR'),
-        description: loan.user_description || 'بدون توضیح',
-        type: loan.type === 'urgent' ? 'ضروری' : 'معمولی',
-        requestNumber: loan.loan_number,
-        guarantors: loan.guarantors_accept === 'accepted' ? 'تأیید شده' : 'نامشخص',
-      })),
-    };
+  
+    const { loans } = response.data;
+
+    return loans.map((loan: any) => ({
+      id: loan.id,
+      name: `${loan.user.first_name} ${loan.user.last_name}`,
+      amount: `${loan.price.toLocaleString()} تومان`,
+      date: new Date(loan.created_at).toLocaleDateString('fa-IR'),
+      description: loan.user_description || 'بدون توضیح',
+      type: loan.type === 'urgent' ? 'ضروری' : 'معمولی',
+      requestNumber: loan.loan_number,
+      guarantors: Array.isArray(loan.guarantors) ? loan.guarantors : [],
+    }));
   } catch (error) {
     console.error('Error fetching loan data:', error);
     if (axios.isAxiosError(error)) {
@@ -224,19 +59,16 @@ const fetchLoans = async () => {
   }
 };
 
-const EveryRequestLoan: React.FC = () => {
+const EveryRequestLoan: React.FC<Loan> = () => {
   const [isUrgent, setIsUrgent] = useState(false);
 
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['loans'],
-    queryFn: fetchLoans,
+    queryKey: ['loans', isUrgent],
+    queryFn: () => fetchLoans(isUrgent),
   });
 
-  const count = data?.count;
-  const users = data?.loans || [];
-
-  console.log('Users:', users);
-
+  const users = data || [];
+ 
   if (isLoading) {
     return (
       <div>
@@ -256,9 +88,9 @@ const EveryRequestLoan: React.FC = () => {
             <RoutRequestLoan />
           </div>
         </div>
-        <div>
-        <EveryLoanAplicationTable />
-          <div className="flex justify-center items-center -mt-5">
+        <div className='w-[200%] '>
+          <EveryLoanAplicationTable />
+          <div className="flex justify-center items-center ">
             <span className="loading loading-dots text-accent loading-lg"></span>
           </div>
         </div>
@@ -292,10 +124,6 @@ const EveryRequestLoan: React.FC = () => {
     );
   }
 
-  const filteredUsers = users.filter(user =>
-    isUrgent ? user.type === 'ضروری' : user.type === 'معمولی'
-  );
-
   return (
     <div>
       <div className="flex justify-between items-center mb-2 mt-10 mr-3">
@@ -318,11 +146,10 @@ const EveryRequestLoan: React.FC = () => {
         </div>
       </div>
       <div className="mt-4">
-        <EveryLoanAplicationTable users={filteredUsers} />
+        <EveryLoanAplicationTable users={users} />
       </div>
     </div>
   );
 };
 
 export default EveryRequestLoan;
-
