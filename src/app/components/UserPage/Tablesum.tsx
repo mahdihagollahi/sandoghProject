987b3cc5128cfc35e796020374cc650037b673c3
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-09-10 21:08:34 +0330

    fix table eshterak redux

--- src/app/components/UserPage/Tablesum.tsx
+++ src/app/components/UserPage/Tablesum.tsx
@@ -1,45 +1,39 @@
 
-
-// "use client";
 // import React from "react";
 // import { useSelector } from "react-redux";
-// import {payment} from '@/src/app/peymentSlice'
 // import { selectPayment } from "@/src/app/peymentSlice"; 
 
-
 // function Tablesum() {
-//   const selectedPayments = useSelector((state) => state.payment.selectPayment);
+//   const selectedPayments = useSelector(selectPayment);
 
 //   return (
-//     <>
-//       <div className="w-11/12 px-3 py-7 shadow-md rounded-md bg-white">
+//     <div className="w-11/12 px-3 py-7 shadow-md rounded-md bg-white">
+//       <div>
+//         <span className="font-bold mr-12">صورت حساب پرداخت شما</span>
+//       </div>
+//       <div className="flex flex-row justify-around w-[500px] mt-10 ">
 //         <div>
-//           <span className="font-bold mr-12">صورت حساب پرداخت شما</span>
+//           <span className="font-bold">شماره</span>
+//           {selectedPayments.map((item, index) => (
+//             <div key={index}>
+//               <div className="mt-3">
+//                 <span>{item.payment}</span>
+//               </div>
+//             </div>
+//           ))}
 //         </div>
-//         <div className="flex flex-row justify-around w-[500px] mt-10 ">
-//           <div>
-//             <span className="font-bold">شماره</span>
-//             {selectedPayments.map((item, index) => (
-//               <div key={index}>
-//                 <div className="mt-3">
-//                   <span>{item.text}</span>
-//                 </div>
+//         <div>
+//           <span className="font-bold mr-1">مبلغ</span>
+//           {selectedPayments.map((item, index) => (
+//             <div key={index} className="bg-white">
+//               <div className="mt-3">
+//                 <input type="text" value={item.payment} readOnly />
 //               </div>
-//             ))}
-//           </div>
-//           <div>
-//             <span className="font-bold mr-1">مبلغ</span>
-//             {selectedPayments.map((item, index) => (
-//               <div key={index} className="bg-white">
-//                 <div className="mt-3">
-//                   <input type="text" value={item.payment} />
-//                 </div>
-//               </div>
-//             ))}
-//           </div>
+//             </div>
+//           ))}
 //         </div>
 //       </div>
-//     </>
+//     </div>
 //   );
 // }
 
@@ -49,34 +43,29 @@
 
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectPayment } from "@/src/app/peymentSlice"; 
 
 function Tablesum() {
-  const selectedPayments = useSelector(selectPayment);
+  const selectedPayments = useSelector((state) => state.payment.selectPayment);
 
   return (
     <div className="w-11/12 px-3 py-7 shadow-md rounded-md bg-white">
       <div>
         <span className="font-bold mr-12">صورت حساب پرداخت شما</span>
       </div>
-      <div className="flex flex-row justify-around w-[500px] mt-10 ">
+      <div className="flex flex-row justify-around w-[500px] mt-10">
         <div>
           <span className="font-bold">شماره</span>
           {selectedPayments.map((item, index) => (
-            <div key={index}>
-              <div className="mt-3">
-                <span>{item.payment}</span>
-              </div>
+            <div key={index} className="mt-3">
+              <span>{item.text}</span>
             </div>
           ))}
         </div>
         <div>
           <span className="font-bold mr-1">مبلغ</span>
           {selectedPayments.map((item, index) => (
-            <div key={index} className="bg-white">
-              <div className="mt-3">
-                <input type="text" value={item.payment} readOnly />
-              </div>
+            <div key={index} className="mt-3">
+              <input type="text" value={item.payment} readOnly />
             </div>
           ))}
         </div>
@@ -86,3 +75,4 @@ function Tablesum() {
 }
 
 export default Tablesum;
+
