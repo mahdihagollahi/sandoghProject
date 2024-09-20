b9c97e1752f8cfa4b2b16d9cdcdc0d6a27763b41
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-09-20 08:16:44 +0330

    fix setting admin page

--- src/app/components/UserPage/RuleAdmin.tsx
+++ src/app/components/UserPage/RuleAdmin.tsx
@@ -1,100 +1,129 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import backImage from "@/src/app/assent/Img/adminPanel/back.svg";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // Import styles
 import posetiv from "@/src/app/assent/Img/userPanel/posetiv.svg";
 import negetiv from "@/src/app/assent/Img/userPanel/negtiv.svg";
+import EditIcon from "@/src/app/assent/Img/adminPanel/EditIconn.svg";
 
 function RuleAdmin() {
-  const [counter, setcounter] = useState(0);
-  const [vam, setvam] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [vam, setVam] = useState(0);
+  const [editorContent, setEditorContent] = useState("");
+
   function posetive() {
-    setcounter(counter + 1);
+    setCounter(counter + 1);
   }
   function negtiv() {
     if (counter > 0) {
-      setcounter(counter - 1);
+      setCounter(counter - 1);
     }
   }
 
   function vamposetive() {
-    setvam(vam + 1);
+    setVam(vam + 1);
   }
   function vamnegtiv() {
     if (vam > 0) {
-      setvam(vam - 1);
+      setVam(vam - 1);
     }
   }
 
-  
   return (
     <>
-      <div className="flex gap-[70%] items-center mb-2 mt-12   ">
-        <div className="mr-2 ">
-          <p className="font-bold text-lg">قوانین ومقررات</p>
-        </div>   
-      </div>
-      <div className="flex flex-col gap-7 p-10 mt-10 w-[90%] shadow-md rounded-md">
-        <div className=" p-5 border-[1px] border-slate-500 flex flex-col gap-8 w-[90%]">
-          <div className="flex flex-col">
-            <span className="font-bold">پرداخت اقساط </span>
-            <p>
-              اقساط اشتراک خود صندوق بی نهایت و بلند مدت است و تا زمانی که
-              کاربران و مدیر بخواهند ادامه پیدا میکند ، اما برای اقساط و میزان
-              اقساط وام هایی که دریافت میکنید توسط مدیر به شما اعلام میشود که چه
-              تعداد و چه میزان باید بپردازید
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold">ضامن</span>
-            <p>
-              پس از تایید درخواست وام شما توسط مدیر ، شما باید 2 نفر از اعضای
-              صندوق را به عنوان ضامن خود معرفی کنید و اسامی آنان را در لیست ضامن
-              ها وارد کنید تا مبلغ وام به شما پرداخت شود .
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold">ضامن خارج از صندوق</span>
-            <p>
-              با توجه به اینکه صندوق قرض الحسنه خانوادگی و دوستانه است و همه ی
-              اعضا یکدیگر را میشناسند باید ضامن نیز از بین از همان اعضا باشد .
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold">مدارک</span>
-            <p>
-              وام تنها مدارکی که نیاز است مدارک و اطلاعات ثبت شده در پروفایل
-              کاربری شماست .
-            </p>
-          </div>
-        </div>
-        <div>
-          <div className=" flex flex-row gap-9">
-            <span>تعداد مجاز ضامن ها برای هر کاربر</span>
-            <div className="flex flex-row gap-4">
-              <Image onClick={() => posetive()} src={posetiv} alt="" />
-              <span>{counter}</span>
-              <Image onClick={() => negtiv()} src={negetiv} alt="" />
-            </div>
-          </div>
-          <div className=" flex flex-row gap-9 mt-[10px]">
-            <span>تعداد وام های مجاز برای هر کاربر</span>
-            <div className="flex flex-row gap-4">
-              <Image onClick={() => vamposetive()} src={posetiv} alt="" />
-              <span>{vam}</span>
-              <Image onClick={() => vamnegtiv()} src={negetiv} alt="" />
-            </div>
-          </div>
-          <div className="flex flex-row gap-4 mt-[10px]">
-            <span>مبلغ حق اشتراک را مشخص کنید</span>
-            <input
-              className="w-28 border-[1px] border-[#CACACA] rounded-md p-2"
-              type="number"
-              placeholder="مبلغ به ریال"
-            />
-          </div>
+      <div className="flex gap-[70%] items-center mb-2 mt-[50px]">
+        <div className="mr-2">
+          <p className="font-bold text-lg">تنظیمات</p>
         </div>
       </div>
+
+      <div className=" px-5 py-10 mt-10 bg-white w-[113%] shadow-md rounded-md">
+        <div className="flex  gap-40">
+          <div>
+            <div className="flex flex-row gap-9">
+              <span>تعداد مجاز ضامن ها برای هر کاربر</span>
+              <div className="flex flex-row gap-4">
+                <Image onClick={() => posetive()} src={posetiv} alt="" />
+                <span>{counter}</span>
+                <Image onClick={() => negtiv()} src={negetiv} alt="" />
+              </div>
+            </div>
+            <div className="flex flex-row gap-9 mt-7">
+              <span>تعداد وام های مجاز برای هر کاربر</span>
+              <div className="flex flex-row gap-4">
+                <Image onClick={() => vamposetive()} src={posetiv} alt="" />
+                <span>{vam}</span>
+                <Image onClick={() => vamnegtiv()} src={negetiv} alt="" />
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-4 mt-7">
+              <span>مبلغ حق اشتراک را مشخص کنید</span>
+              <input
+                className="w-28 border-[1px] border-[#CACACA]  rounded-md p-2"
+                type="number"
+                placeholder="مبلغ به ریال"
+              />
+            </div>
+            <div className="flex items-center gap-2 mt-7 mb-4">
+              <Image src={EditIcon} width={24} height={24} alt="" />
+              <p className="font-demiBold text-lg">قوانین ومقررات</p>
+            </div>
+          </div>
+
+          <div>
+            <p className="font-medium text-[#868686] text-base">
+              شماره کارت را وارد کنید
+            </p>
+            <div className="flex items-center gap-2">
+              <input
+                type="text"
+                className="bg-[#FBFCFB] px-4 w-60 h-10 mt-3 border border-[#DEDDDD] rounded-lg placeholder:text-[#ADADAD] placeholder:font-regular placeholder:text-sm
+            "
+                placeholder="0000-0000-0000-0000"
+              />
+              <button className="bg-[#4FD1C5] mt-3 text-white w-14 py-2 px-3 rounded-md">
+                ثبت
+              </button>
+            </div>
+            <div className="mt-8">
+              <textarea
+                name=""
+                id=""
+                className="w-96 h-20 bg-[#F9F9F9] rounded-lg px-2 py-4 placeholder:text-[#ADADAD] placeholder:font-medium"
+                placeholder="متن یادآوری را بنویسید..."
+              ></textarea>
+            </div>
+            <button className="bg-[#4FD1C5] mt-2 mb-3 text-white w-14 py-2 px-3 rounded-md">
+              ارسال
+            </button>
+          </div>
+        </div>
+
+        <div className="p-5 border-[1px] border-slate-500 flex flex-col gap-8 ">
+          <div>
+            <div className="flex flex-col gap-4 mt-4">
+              <span>ویرایش متن:</span>
+              <ReactQuill
+                value={editorContent}
+                onChange={setEditorContent}
+                placeholder="متن خود را وارد کنید..."
+                className="border-[1px] border-[#CACACA] rounded-md"
+              />
+            </div>
+
+            <div className="mt-4">
+              <span className="font-bold">محتوای ثبت شده:</span>
+              <div className="border p-4 rounded-md border-[#CACACA] mt-2">
+                <div dangerouslySetInnerHTML={{ __html: editorContent }} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="bg-[#4FD1C5] text-white rounded-md p-4 w-[60%] mt-10 whitespace-nowrap">
+        ثبت اطلاعات
+      </button>
     </>
   );
 }
